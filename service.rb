d0d9b9ee84a6b8a44fa061ee492b975d780405f2
alvin2ye <alvin.ye.cn@gmail.com>
2013-12-11 00:33:04 +0800

    use 0.0.0.0

--- service.rb
+++ service.rb
@@ -8,7 +8,7 @@ require 'thin'
 $:.unshift(File.dirname(__FILE__)) 
 require 'app.rb'
 
-server = ::Thin::Server.new('127.0.0.1', 8001, App)
+server = ::Thin::Server.new('0.0.0.0', 8001, App)
 server.log_file = 'tmp/thin.log'
 server.pid_file = 'tmp/thin.pid'
 server.daemonize
